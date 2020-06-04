b74637bbcb8072321522a7089b8558eadc43dee1
EthanHNguyen <ethanhuunguyen@gmail.com>
2020-06-04 15:33:58 -0400

    Working twitter streamer

--- TwitterStream/twitter_streamer.py
+++ TwitterStream/twitter_streamer.py
@@ -3,13 +3,21 @@
 import tweepy
 import twitter_credentials
 
-# 
+'''
+The MyStreamListener classifies twitter messages and routes them to appropiately named methods.
+'''
+class MyStreamListener(tweepy.StreamListener):
+    def on_status(self, status):
+        print(status.text)
+
+# OAuth authorization with Twitter server.
 auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-api.update_profile('Sentiment Analysis Beta Test')
-public_tweets = api.home_timeline()
+# Set-up Twitter stream
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
 
-for tweet in public_tweets:
-    print(tweet.text)
+# Specify how you want to filter the stream
+myStream.filter(track=['COVID'])
