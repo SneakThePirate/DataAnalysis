6cb26612914557cb96c06385e57881a216faafe0
EthanHNguyen <ethanhuunguyen@gmail.com>
2020-06-04 11:33:04 -0400

    Cleaning up test files and folder names

---
+++ TwitterStream/twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contains the user credentials to access Twitter API
+
+ACCESS_TOKEN = '1268285248418299915-eBfrZ9ZC8ygitQ5BXjGq3lu8UnRKeY'
+ACCESS_TOKEN_SECRET = 'WLINytffpyFiuQPxCcz2RmLWYPDXafchuognUw13emRWC'
+CONSUMER_KEY = '1bxdepO8DpuHsDS21qInmR7so'
+CONSUMER_SECRET = 'hxEeT2EO84FRiD0C1t8FqE70JowZpb5qrFddaoXblpU64i3Ym5'
